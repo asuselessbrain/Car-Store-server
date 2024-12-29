dd7c35f717d950097328d7deebbd1239f9c03c6c
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-12-29 18:00:08 +0600

    vercel.json file added

--- src/modules/order/order.route.ts
+++ src/modules/order/order.route.ts
@@ -3,6 +3,7 @@ import { orderController } from './order.controller';
 
 const orderRoute = express.Router();
 
+// orderRoute.post('/create-order', orderController.createOrder);
 orderRoute.post('/create-order', orderController.createOrder);
 orderRoute.get('/revenue', orderController.getRevenue);
 
